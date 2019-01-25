69108278a2f9a6ee3a85aaad4373342e8aaaea0a
Abi Noda <a@abinoda.com>
2019-01-25 08:50:09 -0700

    print error message when secret is not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+if [[ -z "$SLACK_BOT_TOKEN" ]]; then
+  echo "Set the SLACK_BOT_TOKEN secret."
+  exit 1
+fi
+
 curl -X POST \
      -H "Content-type: application/json" \
      -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
